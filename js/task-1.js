f08fa386f51eb04c57e60e25c8ecf1d75af29150
olga <odubasss@gmail.com>
2024-12-21 21:36:55 +0100

    update files

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+function slugify(title) {
+  let result = '';
+  
+  for (let i = 0; i < title.length; i++) {
+    if (title[i] === ' ') {
+      result += '-';  
+    } else {
+      result += title[i].toLowerCase(); 
+    }
+  }
+  
+  return result.trim(); 
+}
+
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
